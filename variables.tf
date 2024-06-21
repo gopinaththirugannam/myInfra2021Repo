6a2e00a9177704474c81c8936ab81be51de61da7
gopinaththirugannam <141162751+gopinaththirugannam@users.noreply.github.com>
2024-06-21 22:02:25 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "tf-infra-keypair1" 
 }
 
 variable "instance_type" { 
